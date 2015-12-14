19717d12daab65f312b7ce160abe5829460dae6e
lauraPolytech <laura.bellego@etu.univ-tours.fr>
2015-12-14 19:06:40 +0100

    Connexion DB

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,37 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.mysql.jdbc.Statement;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Connection conn = null;
+		String url= "jdbc:mysql://localhost:3307/reservationsalle"+
+				"?verifyServerCertificate=false"+
+				"&useSSL=true";
+		String login = "root";
+		String passwd = "password";
+		java.sql.Statement st = null;
+		ResultSet rs=null;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			conn = DriverManager.getConnection(url,login,passwd);
+			
+			st = conn.createStatement();
+			
+		} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
